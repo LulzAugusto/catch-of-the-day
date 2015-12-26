c5bf1647d77d44fa01503997559486becbcf7dd9
Luiz Augusto Crisostomo <luiz66@gmail.com>
2015-12-26 16:18:15 -0300

    Video 8

--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,8 @@ var Route = ReactRouter.Route;
 var Navigator = ReactRouter.Navigator;
 var createBrowserHistory = require('history/lib/createBrowserHistory');
 
+var h = require('./helpers');
+
 /*
     App
  */
@@ -79,7 +81,7 @@ var StorePicker = React.createClass({
         return (
             <form className="store-selector">
                 <h2>Please enter a Store</h2>
-                <input type="text" ref="storeId" required/>
+                <input type="text" ref="storeId" defaultValue={h.getFunName()} required/>
                 <input type="submit"/>
             </form>
         )
